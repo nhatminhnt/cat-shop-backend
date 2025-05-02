a3d4bab39140585c3476978eec556abbf06ff16c
nhatminhnt <nhatminhnt1522@gmail.com>
2025-05-03 02:27:40 +0700

    feat: register, login, JWT

---
+++ src/controllers/auth.controller.ts
@@ -0,0 +1,54 @@
+import { Request, Response } from "express";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import User from "../models/user.model";
+
+const generateToken = (id: string) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET || "secret", {
+    expiresIn: "30d",
+  });
+};
+
+export const register = async (req: Request, res: Response) => {
+  const { name, email, password, role } = req.body;
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    return res.status(400).json({ message: "Email đã tồn tại" });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+    role,
+  });
+
+  res.status(201).json({
+    _id: user.id,
+    name: user.name,
+    email: user.email,
+    role: user.role,
+    token: generateToken(user._id),
+  });
+};
+
+export const login = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+      toke: generateToken(user._id),
+    });
+  } else {
+    res
+      .status(401)
+      .json({ message: "Mail hoặc password của bạn không hợp lệ" });
+  }
+};
---
+++ src/models/user.model.ts
@@ -0,0 +1,20 @@
+import mongoose, { Schema, Document } from 'mongoose';
+
+export interface IUser extends Document {
+  name: string;
+  email: string;
+  password: string;
+  role: 'admin' | 'shop' | 'customer';
+}
+
+const UserSchema: Schema = new Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: ['admin', 'shop', 'customer'], default: 'customer' }
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model<IUser>('User', UserSchema);
